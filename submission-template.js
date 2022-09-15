dfdccf5d8a55c95dd63c0087a9549ca54e7fd8a3
rrthurein <104802546+rrthurein@users.noreply.github.com>
2022-09-15 15:47:05 -0700

    submission-template

--- submission-template.js
+++ submission-template.js
@@ -1,23 +1,69 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
+    let total = 0
+  for(let i = 0; i < array.length; i++){
+      total += array[i]
+};
+    return total;
 };
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
-};
+  const frequency = {};
+  let maxValue = -9999;
+  let minValue = 9999;
+  let minKey = '';
+  let maxKey = '';
+    
+    for(let str of array){
+        if(frequency[str]){
+            frequency[str]++;
+        } else{
+            frequency[str] = 1;
+        }
+    } //this for loop is for the number of occurences which is the key
+  keys = Object.keys(frequency); //returns the array of keys
+  for(let i=0; i<keys.length; i++){
+      let value = frequency[keys[i]] //accessing the value of each key
+    if(minValue > value){
+        minValue = value;
+        minKey = keys[i];
+    } 
+    if(maxValue < value){
+        maxValue = value;
+        maxKey = keys[i];
+    }
+    return {most: maxKey, least: minKey};
+  }
+}
 
-const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+function checkPalindrom (str) {
+ let word = str.toLowerCase().split('').reverse().join('');
+    finalizeWord = word.charAt(0).toUpperCase() + word.slice(1);
+    if(str === finalizeWord){
+        return true
+    } else {
+return false}
 };
 
 const largestPair = function(array) {
-  // your code here - don't forget to return a number!
+  const largestPair = function(array) {
+  let max = 0
+
+for(let i=0; i < array.length; i++){
+    let result = array[i] * array[i + 1]
+    if(max < result){
+        max = result;
+    } 
+}
+return max;
+};
 };
 
-const removeParenth = function(str) {
-  // your code here - don't forget to return a string!
+
+  const removeParenth = function(str) {
+  return str.replace(/ *\([^)]*\) */g, "");
 };
+
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+ 
 };
